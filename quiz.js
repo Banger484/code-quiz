d54c635690f5558fc8150197cde3d8f008218370
Bret Banger <Banger484@gmail.com>
2022-03-29 09:55:27 -0500

    bunch of random functionality created

---
+++ quiz.js
@@ -0,0 +1,30 @@
+var quiz;
+var quizPool = [
+    {
+      question: "Which tag is used to link a CSS file to an html document?",
+      answers: ["<link>", "<style>", "<css>", "<style-sheet>"],
+      correct: "<link>",
+    },{
+      question: "How do you change font color with CSS?",
+      answers: ["color:", "font-color:", "text-color:", "colorful-words:"],
+      correct: "color:", 
+    },
+    {
+      question: "Why though?",
+      answers: ["because", "none-ya", "dunno", "no you!"],
+      correct: "because",
+    },{
+      question: "Why though?",
+      answers: ["because", "none-ya", "dunno", "no you!"],
+      correct: "because", 
+    },
+    {
+      question: "Why though?",
+      answers: ["because", "none-ya", "dunno", "no you!"],
+      correct: "because",
+    },{
+      question: "Why though?",
+      answers: ["because", "none-ya", "dunno", "no you!"],
+      correct: "because", 
+    },
+  ];
--- script.js
+++ script.js
@@ -1,51 +1,63 @@
-var quiz = {
-    question: "Why though?",
-    answers: ["because", "none-ya", "dunno", "no you!"],
-    correct: "because"
-}
-var displayAnswers;
+var startBtn = document.getElementById("start-btn");
+var displayQuestion = document.getElementById("quiz-question");
+var quizDisplay = document.getElementById("quiz-display");
+var displayAnswers = document.getElementById("quiz-answers");
+var timer = 60;
 
-var quizDisplay = document.createElement("div")
-document.body.appendChild(quizDisplay)
+function quizGame() {
+  if (startBtn.style.display === "none") {
+    startBtn.style.display = "block";
+  } else {
+    startBtn.style.display = "none";
+  }
 
-
-
-
-function quizGame () {
-    var displayQuestion = document.createElement("h1")
-    displayQuestion.textContent = quiz.question
-    quizDisplay.appendChild(displayQuestion)
-    displayAnswers = document.createElement("ul")
-    displayAnswers.setAttribute('id', 'choices')
-    quizDisplay.appendChild(displayAnswers)
-
-    displayMultipleChoice()
-    
+  displayQuiz();
 }
 
-quizGame()
-
-
-function displayMultipleChoice () {
-    for(let i = 0; i < 4; i++) {
-        var index = Math.floor(Math.random() * quiz.answers.length)
-        var displayAnswer = document.createElement('li')
-        displayAnswer.setAttribute('class', 'answers')
-        displayAnswers.appendChild(displayAnswer)
-        displayAnswer.textContent = quiz.answers[index]
-        displayAnswer.setAttribute('id', quiz.answers[index])
-        quiz.answers.splice(index, 1)
+function displayQuiz() {
+    if(quizPool.length === 0 || timer === 0) {
+        removeQuiz(quizDisplay)
+        gameOver()
+        if (startBtn.style.display === "none") {
+            startBtn.style.display = "block";
+          } else {
+            startBtn.style.display = "none";
+          }
     }
+  var index = Math.floor(Math.random() * quizPool.length);
+  quiz = quizPool[index];
+  quizPool.splice(index, 1);
+  displayQuestion.textContent = quiz.question;
+  removeQuiz(displayAnswers);
+  for (let i = 0; i < 4; i++) {
+    quizDisplay.appendChild(displayAnswers);
+    var index = Math.floor(Math.random() * quiz.answers.length);
+    var displayAnswer = document.createElement("li");
+    displayAnswer.setAttribute("class", "answers");
+    displayAnswers.appendChild(displayAnswer);
+    displayAnswer.textContent = quiz.answers[index];
+    displayAnswer.setAttribute("id", quiz.answers[index]);
+    quiz.answers.splice(index, 1);
+  }
+
 }
 
-function checkAnswer (e) {
-    
-    if (e.path[0].id === quiz.correct) {
-        console.log("Correct!!!")
-    } else {
-        console.log("Hell naw!!!");
-    }
+function checkAnswer(e) {
+  if (e.path[0].id === quiz.correct) {
+    displayQuiz();
+  } else {
+    console.log("Hell naw!!!");
+  }
 }
 
+function removeQuiz(p) {
+  while (p.firstChild) {
+    p.removeChild(p.firstChild);
+  }
+}
+function gameOver () {
+    console.log("GameOver");
+}
 
-  displayAnswers.addEventListener('click', checkAnswer);
+startBtn.addEventListener("click", quizGame);
+displayAnswers.addEventListener("click", checkAnswer);
